0ba4fbe54101f98de80832f0c955c7cb97f90175
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-05-27 06:57:48 -0400

    ssl to generate with no passphrase
    pulled from https://serverfault.com/questions/366372/is-it-possible-to-generate-rsa-key-without-pass-phrase

--- scripts/55-install-SSL-cert.sh
+++ scripts/55-install-SSL-cert.sh
@@ -4,8 +4,7 @@ apt install letsencrypt net-tools
 #certbot certonly
 echo "set FQDN , hostname @ prompt Common Name (e.g. server FQDN or YOUR name) []:skytune.online.io      < replace "
 
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
-
+openssl req  -nodes -new -x509  -keyout server.key -out server.cert
 
 
 
