8256293cc6f57cefb7cb299d120912e3da6f4dad
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-05-27 06:42:10 -0400

    Update install-SKYTUNE-server.sh

--- scripts/install-SKYTUNE-server.sh
+++ scripts/install-SKYTUNE-server.sh
@@ -9,4 +9,10 @@ sh 33_create-aws-user.sh
 sh 44_rails-DB-setup.sh
 cd ../
 apt install letsencrypt netstat
-certbot certonly
+#certbot certonly
+echo "set FQDN , hostname @ prompt Common Name (e.g. server FQDN or YOUR name) []:skytune.online.io      < replace "
+
+openssl req -new > cert.csr
+openssl rsa -in privkey.pem -out key.pem
+openssl x509 -in cert.csr -out cert.pem -req -signkey key.pem -days 1001
+cat key.pem>>cert.pem
