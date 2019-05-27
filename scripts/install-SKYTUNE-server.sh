44b23539f8cb735960548db141c1256248d36ea6
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-05-27 06:10:31 -0400

    Update install-SKYTUNE-server.sh

--- scripts/install-SKYTUNE-server.sh
+++ scripts/install-SKYTUNE-server.sh
@@ -6,9 +6,11 @@ echo " run postgreSQL install 22_setup-andcreate-postgresand-database"
 echo " display aws readme for s3 install 33_create-aws-user.sh"
 echo " run install for rails, db and main server 44_rails-DB-setup.sh"
 
-sh 11_install-node-9.8.0
-sh 22_setup-andcreate-postgresand-database
+sh 11_install-node-9.8.0.sh
+sh 22_setup-andcreate-postgresand-database.sh
 sh 33_create-aws-user.sh
 sh 44_rails-DB-setup.sh
 cd ../
 apt install letsencrypt
+
+
